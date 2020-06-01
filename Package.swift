698fdda078fceb6801c5e799fa637d8c8c8ab7fe
Greg Fajen <gregfajen@gmail.com>
2020-06-01 18:27:40 -0400

    added products

--- Package.swift
+++ Package.swift
@@ -3,22 +3,23 @@ import PackageDescription
 
 let package = Package(
     
-    name: "Protos",
+    name: "Plane",
     
     platforms: [
         .macOS(.v10_15)
     ],
     
+    products: [
+        .library(name: "Plane", targets: ["plane"])
+    ],
+    
     dependencies: [
         .package(url: "https://github.com/grpc/grpc-swift.git",
-                 from: "1.0.0-alpha.12"),
-//        .package(name: "AWSSDKSwift",
-//                    url: "https://github.com/swift-aws/aws-sdk-swift.git",
-//                 from: "4.6.0")
+                 from: "1.0.0-alpha.12")
     ],
     
     targets: [
-
+        
         .target(
             name: "Protos",
             dependencies: [
@@ -26,15 +27,14 @@ let package = Package(
             ]
         ),
         
-//        .target(
-//            name: "Plane",
-//            dependencies: [
-//                "Protos",
-//                .product(name: "GRPC", package: "grpc-swift"),
-////                .product(name: "EC2", package: "AWSSDKSwift")
-//            ]
-//        )
+        .target(
+            name: "Plane",
+            dependencies: [
+                "Protos",
+                .product(name: "GRPC", package: "grpc-swift")
+            ]
+        )
         
     ]
     
-)
+)s
